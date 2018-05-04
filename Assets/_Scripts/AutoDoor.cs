1aafdd79f64b0dbe916d2b8cfffd7447f27334c2
Sean Harrison <seanharrs97@hotmail.com>
2018-05-04 15:42:52 +1000

    Basic security camera and automatic door system

---
+++ Assets/_Scripts/AutoDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_OpenPos;
+
+    [SerializeField]
+    private Vector3 m_ClosePos;
+
+    private bool m_IsOpen = false;
+    public bool isOpen
+    {
+        get { return m_IsOpen; }
+        private set { m_IsOpen = value; }
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(isOpen);
+        if(isOpen && transform.position != m_OpenPos)
+            transform.position = Vector3.MoveTowards(transform.position, m_OpenPos, Time.fixedDeltaTime);
+        else if(!isOpen && transform.position != m_ClosePos)
+            transform.position = Vector3.MoveTowards(transform.position, m_ClosePos, Time.fixedDeltaTime);
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+    }
+
+    public void CloseDoor()
+    {
+        isOpen = false;
+    }
+}
---
+++ Assets/_Scripts/SecurityCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    private AutoDoor connectedDoor;
+    private int m_EntitiesInView = 0;
+
+    private void Awake()
+    {
+        connectedDoor = FindObjectOfType<AutoDoor>();
+        connectedDoor.OpenDoor();
+    }
+
+    private void Update()
+    {
+        if(m_EntitiesInView > 0 && connectedDoor.isOpen)
+            connectedDoor.CloseDoor();
+        else if(m_EntitiesInView == 0 && !connectedDoor.isOpen)
+            connectedDoor.OpenDoor();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ++m_EntitiesInView;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        --m_EntitiesInView;
+    }
+}
