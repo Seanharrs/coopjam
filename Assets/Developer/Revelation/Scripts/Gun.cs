dda320a034862d6f714d7f14b04ca7845269a8d6
jamesgmarks <jamesgmarks@gmail.com>
2018-05-04 19:17:57 -0400

    Character controller and supporting items.

---
+++ Assets/Developer/Revelation/Scripts/Gun.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+  public enum WhichWeapon
+  {
+    Primary,
+    Secondary
+  }
+  public class Gun : MonoBehaviour
+  {
+
+    private Dictionary<WhichWeapon, float> m_LastFired = new Dictionary<WhichWeapon, float> {
+      { WhichWeapon.Primary, -5 },
+      { WhichWeapon.Secondary, -5 }
+    };
+
+    [Header("Details")]
+    [Tooltip("Number of times you can fire this weapon in one second.")]
+    public float FiringRate = 5;
+
+    [Header("Prefabs")]
+    public Projectile PrimaryAmmoType;
+    public Projectile SecondaryAmmoType;
+
+    [Header("Game Objects")]
+    public Transform AmmoSpawnLocation;
+
+    public bool Fire(WhichWeapon weapType, Vector2? direction = null)
+    {
+      if (Time.time > m_LastFired[weapType] + (1/FiringRate))
+      {
+        // TODO: Spawn projectile.
+        if (AmmoSpawnLocation && PrimaryAmmoType)
+        {
+          var projectile = Instantiate(weapType == WhichWeapon.Primary ? PrimaryAmmoType : SecondaryAmmoType, AmmoSpawnLocation.position, Quaternion.identity);
+          if (projectile) {
+            projectile.Initiate(direction != null ? (Vector2)direction : (Vector2)AmmoSpawnLocation.localScale * AmmoSpawnLocation.right);
+            m_LastFired[weapType] = Time.time;
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+  }
---
+++ Assets/Developer/Revelation/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,168 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets._2D;
+// using UnityStandardAssets.CrossPlatformInput;
+
+namespace Coop
+{
+  // TODO: Flags makes it possible to implement dual input mode (since I've seen it done in Unreal).
+  //       If we don't find a use for this, remove Flags attribute.
+  [Flags, Serializable]
+  public enum PlayerInputMode
+  {
+    Game = 1,
+    UI = 2
+  }
+  [RequireComponent(typeof(PlatformerCharacter2D))]
+  public class Platformer2DUserControl : MonoBehaviour
+  {
+    private PlatformerCharacter2D m_Character;
+    private bool m_Jump;
+    private PlayerInputMode m_InputMode = PlayerInputMode.Game;
+
+    [Header("Weapon")]
+    public Gun gun;
+    public GameObject gunSocket;
+
+    [Header("Gameplay Axes")]
+    public string horizontalAxis = "Horizontal_P1";
+    public string aimHorizontal = "AimHoriz_P1";
+    public string aimVertical = "AimVert_P1";
+
+    [Header("Gameplay Buttons")]
+    public string jump = "Jump_P1";
+    public string crouchButton = "Crouch_P1";
+    public string primaryFire = "FirePrimary_P1";
+    public string secondaryFire = "FireSecondary_P1";
+    public string aimActivate = "AimActivate_P1";
+    public string switchPlayerWeapon = "Switch_P1";
+    public string interact = "Interact_P1";
+
+    [Header("Menu Buttons")]
+    public string openMenuPause = "MenuPause_P1";
+    public string submitButton = "Submit_P1";
+    public string cancelButton = "Cancel_P1";
+
+    private void Awake()
+    {
+      m_Character = GetComponent<PlatformerCharacter2D>();
+    }
+
+
+    private void Update()
+    {
+      if (!m_Jump)
+      {
+        // Read the jump input in Update so button presses aren't missed.
+        m_Jump = Input.GetButtonDown(jump);
+      }
+
+      //TODO: Package this and move it to an appropriate script/method
+      #region Manage Game Input
+      if (m_InputMode == PlayerInputMode.Game)
+      {
+
+        float verticalAim = Input.GetAxis(aimVertical);
+        if (Mathf.Abs(verticalAim) > 0) {
+          gunSocket.transform.Rotate(0, 0, verticalAim);
+          var zRotation = gunSocket.transform.rotation.eulerAngles.z;
+          if(zRotation > 90 && zRotation < 270) {
+            if(zRotation < 180) zRotation = 90; else zRotation = 270;
+            gunSocket.transform.rotation = Quaternion.Euler(0, 0, zRotation);
+          }
+        }
+
+        // manage game controls
+        if (Input.GetButtonDown(jump))
+        {
+          Debug.Log("Pressed: jump");
+        }
+        else if (Input.GetButtonDown(crouchButton))
+        {
+          Debug.Log("Pressed: crouchButton");
+        }
+        else if (Input.GetAxis(primaryFire) != 0)
+        {
+          Debug.Log("Pressed: Fire " + Input.GetAxis(primaryFire));
+          gun.Fire(Input.GetAxis(primaryFire) > 0 ? WhichWeapon.Primary : WhichWeapon.Secondary, gunSocket.transform.right * Mathf.Sign(transform.localScale.x));
+        }
+        else if (Input.GetButtonDown(primaryFire))
+        {
+          Debug.Log("Pressed: Primary Fire");
+          gun.Fire(WhichWeapon.Primary);
+        }
+        else if (Input.GetButtonDown(secondaryFire))
+        {
+          Debug.Log("Pressed: Secondary Fire");
+          gun.Fire(WhichWeapon.Secondary);
+        }
+        else if (Input.GetButtonDown(aimActivate))
+        {
+          Debug.Log("Pressed: aimActivate");
+        }
+        else if (Input.GetButtonDown(switchPlayerWeapon))
+        {
+          Debug.Log("Pressed: switchPlayerWeapon " + Input.GetAxis(switchPlayerWeapon));
+        }
+        else if (Input.GetButtonDown(interact))
+        {
+          Debug.Log("Pressed: interact");
+        }
+        else if (Input.GetButtonDown(openMenuPause))
+        {
+          Debug.Log("Pressed: Pause");
+          Time.timeScale = 0;
+          // TODO: Show pause menu
+          SetInputMode(Coop.PlayerInputMode.UI);
+        }
+      }
+      #endregion
+
+      //TODO: Package this and move it to an appropriate script/method
+      #region Manage UI Input
+      else if (m_InputMode == PlayerInputMode.UI)
+      {
+        // manage UI controls
+        if (Input.GetButtonDown(openMenuPause))
+        { // && !canGoBack
+          // Close menu
+          SetInputMode(PlayerInputMode.Game);
+          Time.timeScale = 1;
+          Debug.Log("Pressed: Unpause");
+        }
+        else if (Input.GetButtonDown(submitButton))
+        {
+          // Activate menu item
+          Debug.Log("Pressed: submitButton");
+        }
+        else if (Input.GetButtonDown(cancelButton))
+        { // && canGoBack
+          // Go back
+          Debug.Log("Pressed: cancelButton");
+        }
+      }
+      #endregion
+
+    }
+
+    private void FixedUpdate()
+    {
+      // Read the inputs.
+      bool crouch = Input.GetButton(crouchButton);
+      float h = Input.GetAxis(horizontalAxis);
+      // Pass all parameters to the character control script.
+      m_Character.Move(h, crouch, m_Jump);
+      m_Jump = false;
+    }
+
+    public void SetInputMode(PlayerInputMode mode)
+    {
+      m_InputMode = mode;
+    }
+    public PlayerInputMode GetInputMode()
+    {
+      return m_InputMode;
+    }
+
+  }
+}
