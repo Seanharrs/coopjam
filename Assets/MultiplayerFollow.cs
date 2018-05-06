f0f363a762ca9f52922dc82720f27fea6d7b7a41
Sean Harrison <seanharrs97@hotmail.com>
2018-05-06 17:15:24 +1000

    Simple multiplayer follow camera using LINQ

---
+++ Assets/MultiplayerFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MultiplayerFollow : MonoBehaviour
+{
+    private Coop.Platformer2DUserControl[] players;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        players = FindObjectsOfType<Coop.Platformer2DUserControl>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 avgPos = players.Select(p => p.transform.position).Aggregate((total, next) => total += next) / players.Length;
+        transform.position = avgPos + offset;
+    }
+}
