c1e0e1dee755ff88582e2baa51b84442df1314bf
Sean Harrison <seanharrs97@hotmail.com>
2018-05-07 00:07:12 +1000

    Account for sprite bounds when clamping to camera view

--- Assets/Developer/Revelation/Scripts/Platformer2DUserControl.cs
+++ Assets/Developer/Revelation/Scripts/Platformer2DUserControl.cs
@@ -27,12 +27,22 @@ namespace Coop
     [Header("Weapon")]
     public Gun gun;
     public GameObject gunSocket;
+    
+    private float m_VertLim;
+    private float m_HorizLim;
+    private float m_BoundX;
+    private float m_BoundY;
 
     private void Awake()
     {
       m_Character = GetComponent<PlatformerCharacter2D>();
       Cursor.lockState = CursorLockMode.Locked;
       Cursor.visible = false;
+
+      m_VertLim = Camera.main.orthographicSize;
+      m_HorizLim = m_VertLim * Screen.width / Screen.height;
+      m_BoundX = GetComponent<SpriteRenderer>().sprite.bounds.extents.x;
+      m_BoundY = GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
     }
 
 
@@ -181,13 +191,8 @@ namespace Coop
     {
       Vector3 pos = transform.position;
       Vector3 camPos = Camera.main.transform.position;
-      float vertLim = Camera.main.orthographicSize;
-      float horizLim = vertLim * Screen.width / Screen.height;
-
-      pos.x = Mathf.Clamp(pos.x, camPos.x - horizLim, camPos.x + horizLim);
-      pos.y = Mathf.Clamp(pos.y, camPos.y - vertLim, camPos.y + vertLim);
-      
-      //TODO take into account bounds of player sprite
+      pos.x = Mathf.Clamp(pos.x, camPos.x - m_HorizLim + m_BoundX, camPos.x + m_HorizLim - m_BoundX);
+      pos.y = Mathf.Clamp(pos.y, camPos.y - m_VertLim + m_BoundY, camPos.y + m_VertLim - m_BoundY);
 
       //TODO kill player if they fall off the bottom of the camera?
       //     how do we then handle reverse gravity sending player off the top?
