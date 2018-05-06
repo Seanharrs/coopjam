2e0cbdcc0b17271836c50fea81270207be6884ef
Sean Harrison <seanharrs97@hotmail.com>
2018-05-06 17:34:01 +1000

    Clamp camera position to be within the defined map limits

--- Assets/MultiplayerFollow.cs
+++ Assets/MultiplayerFollow.cs
@@ -5,19 +5,49 @@ using System.Linq;
 
 public class MultiplayerFollow : MonoBehaviour
 {
-    private Coop.Platformer2DUserControl[] players;
+    private Coop.Platformer2DUserControl[] m_Players;
 
     [SerializeField]
-    private Vector3 offset;
+    private Vector3 m_Offset;
+
+    [SerializeField]
+    [Tooltip("The X coordinate of the left-most tile in the map")]
+    private float m_MinMapX;
+
+    [SerializeField]
+    [Tooltip("The X coordinate of the right-most tile in the map")]
+    private float m_MaxMapX;
+
+    [SerializeField]
+    [Tooltip("The Y coordinate of the lowest tile in the map")]
+    private float m_MinMapY;
+
+    [SerializeField]
+    [Tooltip("The Y coordinate of the highest tile in the map")]
+    private float m_MaxMapY;
+
+    private float m_MinCamX, m_MaxCamX, m_MinCamY, m_MaxCamY;
 
     private void Awake()
     {
-        players = FindObjectsOfType<Coop.Platformer2DUserControl>();
+        m_Players = FindObjectsOfType<Coop.Platformer2DUserControl>();
+
+        float vertCamLen = GetComponent<Camera>().orthographicSize;
+        float horizCamLen = vertCamLen * Screen.width / Screen.height;
+
+        m_MinCamX = m_MinMapX + horizCamLen;
+        m_MaxCamX = m_MaxMapX - horizCamLen;
+        m_MinCamY = m_MinMapY + vertCamLen;
+        m_MaxCamY = m_MaxMapY - vertCamLen;
     }
 
     private void LateUpdate()
     {
-        Vector3 avgPos = players.Select(p => p.transform.position).Aggregate((total, next) => total += next) / players.Length;
-        transform.position = avgPos + offset;
+        Vector3 avgPos = m_Players.Select(p => p.transform.position).Aggregate((total, next) => total += next) / m_Players.Length;
+
+        Vector3 clamped = avgPos + m_Offset;
+        clamped.x = Mathf.Clamp(clamped.x, m_MinCamX, m_MaxCamX);
+        clamped.y = Mathf.Clamp(clamped.y, m_MinCamY, m_MaxCamY);
+        transform.position = clamped;
     }
 }
