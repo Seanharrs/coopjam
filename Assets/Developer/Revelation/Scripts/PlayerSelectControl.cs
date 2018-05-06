e4c21cc18cb77044c81af22b028b2cfef8ceb8d9
jamesgmarks <jamesgmarks@gmail.com>
2018-05-05 21:36:14 -0400

    Updates to control data. Added player portraits. Began work on player select menus.

---
+++ Assets/Developer/Revelation/Scripts/PlayerSelectControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Coop
+{
+  public class PlayerSelectControl : MonoBehaviour
+  {
+
+    public int playerIndex = -1;
+
+    public bool isReady = false;
+
+    public Button leftButton;
+    public Button rightButton;
+    public Image portraitImage;
+    public Dropdown controllerDropdown;
+    public Button readyButton;
+
+  }
+}
