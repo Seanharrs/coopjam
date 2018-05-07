96336304cde142b506f787c1e12d0197d46ea74a
Sean Harrison <seanharrs97@hotmail.com>
2018-05-07 13:45:48 +1000

    Add primary and secondary gravity gun effects

---
+++ Assets/Developer/Seanharrs/_Scripts/GravityProjectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Projectile))]
+public class GravityProjectile : MonoBehaviour
+{
+    private enum ProjectileType { Primary, Secondary };
+
+    [SerializeField]
+    private ProjectileType m_Type;
+
+    [SerializeField]
+    private float m_EffectLength;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Rigidbody2D rb2d = collision.GetComponent<Rigidbody2D>();
+        if(rb2d == null)
+        { 
+            Destroy(gameObject);
+            return;
+        }
+
+        if(m_Type == ProjectileType.Primary)
+            StartCoroutine(ReverseGravity(rb2d));
+        else
+            StartCoroutine(ReduceGravity(rb2d));
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+    private IEnumerator ReverseGravity(Rigidbody2D ctrl)
+    {
+        ctrl.gravityScale *= -1;
+        yield return new WaitForSeconds(m_EffectLength);
+        ctrl.gravityScale *= -1;
+    }
+
+    private IEnumerator ReduceGravity(Rigidbody2D ctrl)
+    {
+        ctrl.gravityScale /= 2;
+        yield return new WaitForSeconds(m_EffectLength);
+        ctrl.gravityScale *= 2;
+    }
+}
