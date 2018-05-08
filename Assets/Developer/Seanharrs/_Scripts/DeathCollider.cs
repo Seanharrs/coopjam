306edfeed56f740b8877531b93bf9ecb1c85ff77
Sean Harrison <seanharrs97@hotmail.com>
2018-05-08 14:59:29 +1000

    Add player death and respawning

---
+++ Assets/Developer/Seanharrs/_Scripts/DeathCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DeathCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<DeathAnimation>())
+            return;
+
+        Health hp = other.GetComponent<Health>();
+        if(hp)
+            hp.ForceKill();
+    }
+}
