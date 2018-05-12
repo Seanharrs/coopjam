53e6f2f24d006507ebf9ec31516ab2e94b89de3d
James Marks <jamesgmarks@gmail.com>
2018-05-11 22:41:35 -0400

    Renamed WorkshopGoal to LevelGoal and changed functionality to ensure all players are inside to complete level.

---
+++ Assets/Developer/Revelation/_Scripts/LevelGoal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Coop 
+{
+  public class LevelGoal : MonoBehaviour {
+
+    List<Platformer2DUserControl> overlappedPlayers = new List<Platformer2DUserControl>();
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.GetComponent<Platformer2DUserControl>())
+      {
+        if(AddUnique(other.GetComponent<Platformer2DUserControl>()))
+        {
+          if(CoopGameManager.instance.playerData.Count() == overlappedPlayers.Count())
+          {
+            Debug.Log("All players overlapping");
+            FindObjectOfType<LevelManager>().LevelComplete();
+          }
+        }
+      }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+      if (other.GetComponent<Platformer2DUserControl>())
+      {
+        overlappedPlayers.Remove(other.GetComponent<Platformer2DUserControl>());
+      }
+    }
+
+    bool AddUnique(Platformer2DUserControl controller)
+    {
+      if(!overlappedPlayers.Contains(controller))
+      {
+        overlappedPlayers.Add(controller);
+        return true;
+      }
+      return false;
+    }
+  }
+}
