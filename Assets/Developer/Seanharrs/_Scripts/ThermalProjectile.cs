e3ebd385da6faa7e86254ff62f6ee7c4abdd697f
Sean Harrison <seanharrs97@hotmail.com>
2018-05-12 12:37:16 +1000

    Add thermodynamic gun that can freeze or melt ThermalSensitive objects

---
+++ Assets/Developer/Seanharrs/_Scripts/ThermalProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coop
+{
+    [RequireComponent(typeof(Projectile))]
+    public class ThermalProjectile : MonoBehaviour
+    {
+        [SerializeField]
+        private Projectile.ProjectileType m_Type;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            ThermalSensitive thermalObj = collision.GetComponent<ThermalSensitive>();
+            if(thermalObj != null)
+            {
+                if(m_Type == Projectile.ProjectileType.Primary)
+                    thermalObj.Freeze();
+                else
+                    thermalObj.Melt();
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Developer/Seanharrs/_Scripts/ThermalSensitive.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D))]
+public class ThermalSensitive : MonoBehaviour
+{
+    private enum ThermalState { Melted, Frozen };
+
+    [SerializeField]
+    private Sprite m_FrozenSprite;
+
+    [SerializeField]
+    private Sprite m_MeltedSprite;
+
+    [SerializeField]
+    private ThermalState m_State = ThermalState.Melted;
+
+    private SpriteRenderer m_Renderer;
+    private BoxCollider2D m_Coll;
+
+    private void Awake()
+    {
+        m_Renderer = GetComponent<SpriteRenderer>();
+        m_Coll = GetComponent<BoxCollider2D>();
+        m_Coll.isTrigger = m_State == ThermalState.Melted;
+    }
+
+    public void Freeze()
+    {
+        if(m_State == ThermalState.Frozen)
+            return;
+
+        m_State = ThermalState.Frozen;
+        m_Renderer.sprite = m_FrozenSprite;
+        m_Coll.isTrigger = false;
+    }
+
+    public void Melt()
+    {
+        if(m_State == ThermalState.Melted)
+            return;
+
+        m_State = ThermalState.Melted;
+        m_Renderer.sprite = m_MeltedSprite;
+        m_Coll.isTrigger = true;
+    }
+}
