262b75884215ef279482fcc2b3db5135e5c261e7
Sean Harrison <seanharrs97@hotmail.com>
2018-05-15 14:59:35 +1000

    Clean up LevelSelect.cs
    Convert Level class to be a struct
    Add buildIndex property to Level struct
    Simplify duplicate check LINQ

--- Assets/Developer/Seanharrs/_Scripts/LevelSelect.cs
+++ Assets/Developer/Seanharrs/_Scripts/LevelSelect.cs
@@ -7,9 +7,10 @@ using UnityEngine.UI;
 public class LevelSelect : MonoBehaviour
 {
     [System.Serializable]
-    private class Level
+    private struct Level
     {
         public string name;
+        public int buildIndex;
         public Sprite preview;
     }
 
@@ -22,20 +23,27 @@ public class LevelSelect : MonoBehaviour
     [SerializeField]
     private Level[] m_Levels;
 
-    private int m_Index;
+    private int m_Index = 0;
 
     private void Awake()
     {
-        var lvls = m_Levels.Select(l1 => new { l1.name, count = m_Levels.Where(l2 => l2.name == l1.name).Count() });
-        foreach(var lvl in lvls.Where(l => l.count > 1))
-            Debug.LogError("Level \"" + lvl.name + "\" cannot appear in the Levels array more than once.");
+        IEnumerable<string> duplicateNames = 
+            m_Levels.Where(l1 => m_Levels.Where(l2 => l1.name == l2.name || l1.buildIndex == l2.buildIndex).Count() > 1)
+                    .Select(l => l.name)
+                    .Distinct();
 
-        m_Index = 0;
+        foreach(string level in duplicateNames)
+            Debug.LogError("Duplicate scene entry by name or build index: \"" + level + "\".");
+
+        if(m_Levels.Length == 0)
+            Debug.LogError("Level Select requires at least one level in the array.");
+
+        SetLevel(m_Index);
     }
 
     public void LoadSelectedLevel()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(m_Levels[m_Index].name);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(m_Levels[m_Index].buildIndex);
     }
 
     public void NextLevel()
