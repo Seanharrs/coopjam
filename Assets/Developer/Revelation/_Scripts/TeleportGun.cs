529e6bc5b988a9b79b5b678e361442dff79e9c45
James Marks <jamesgmarks@gmail.com>
2018-05-19 00:19:22 -0400

    Teleport Gun changes
    * Teleport gun now tries to adjust location of portal spawns and spawn location for teleported object/character so that they don't intersect with other colliders.

--- Assets/Developer/Revelation/_Scripts/TeleportGun.cs
+++ Assets/Developer/Revelation/_Scripts/TeleportGun.cs
@@ -11,6 +11,8 @@ namespace Coop
     [Header("Teleport Gun specific properties")]
     [SerializeField]
     private GameObject m_TargetLocationPrefab;
+    [SerializeField]
+    private int m_NumCollisionPasses = 3;
 
     // The thing that will be teleported.
     private GameObject m_TargetObject;
@@ -53,6 +55,11 @@ namespace Coop
         m_TargetLocationObject = Instantiate(m_TargetLocationPrefab, targetLocation, Quaternion.identity);
       else
         m_TargetLocationObject.transform.position = targetLocation;
+
+      for(var i = 0; i < m_NumCollisionPasses; i++)
+      {
+        AdjustForCollisions(m_TargetLocationObject.GetComponent<BoxCollider2D>());
+      }
       TryCompleteTeleport();
     }
 
@@ -63,6 +70,10 @@ namespace Coop
         // Debug.Log("Completing teleport to " + m_TargetLocation.ToString());
         // TODO: Play sound, animation, effect, etc.
         m_TargetObject.transform.position = (Vector2)m_TargetLocation;
+        for(var i = 0; i < m_NumCollisionPasses; i++)
+        {
+          AdjustForCollisions(m_TargetObject.GetComponent<BoxCollider2D>());
+        }
 
         // Debug.Log("Nullifying stuff.");
         m_TargetLocation = null;
@@ -76,6 +87,50 @@ namespace Coop
       //   Debug.Log("Can't complete teleport");
       }
     }
+
+    private void AdjustForCollisions(Collider2D collider)
+    {
+      //var collider = m_TargetLocationObject.GetComponent<BoxCollider2D>();
+      var colliderCenter = (Vector2)collider.transform.position + collider.offset;
+      Collider2D[] results = new Collider2D[10];
+      int overlappedCount = collider.OverlapCollider(new ContactFilter2D(), results);
+      if (overlappedCount > 0)
+      {
+        foreach (var c in results)
+        {
+          Debug.Log("Checking... " + c);
+          if(!c) return;
+          if (!c.isTrigger && c != collider)
+          {
+            var cCenter = (Vector2)c.transform.position + c.offset;
+
+            var desiredDistances = new Vector2(collider.bounds.extents.x + c.bounds.extents.x, collider.bounds.extents.y + c.bounds.extents.y);
+            var directionVector = colliderCenter - cCenter;
+            Debug.Log("Desired Distances: " + desiredDistances);
+
+            var actualDistance2D = (colliderCenter - cCenter);
+            Debug.Log("Actual Distances: " + actualDistance2D);
+
+            var p = collider.transform.position;
+            Debug.Log("Original Position: " + p);
+            if(Mathf.Abs(actualDistance2D.x) < Mathf.Abs(actualDistance2D.y))
+            {
+              Debug.Log("Adjusting on X... " + c);
+              p.x = cCenter.x + (desiredDistances.x * Mathf.Sign(directionVector.x));
+            }
+            else
+            {
+              Debug.Log("Adjusting on Y... " + c);
+              p.y = cCenter.y + (desiredDistances.y * Mathf.Sign(directionVector.y));
+            }
+            Debug.Log("New position: " + p);
+            collider.transform.position = p;
+
+          }
+        }
+      }
+    }
+
 
     /// <summary>
     /// This function is called when the MonoBehaviour will be destroyed.
