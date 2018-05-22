ec07c407cac3579e1e34d04fcb24db90bf191c8e
Sean Harrison <seanharrs97@hotmail.com>
2018-05-22 11:59:08 +1000

    Projectiles collide with lasers, NonDeadlyLaser works stand-and-stay now

--- Assets/_Scripts/NonDeadlyLaser.cs
+++ Assets/_Scripts/NonDeadlyLaser.cs
@@ -1,68 +1,65 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Coop
 {
+	[RequireComponent(typeof(BoxCollider2D), typeof(CircuitObject), typeof(AudioSource))]
+	public class NonDeadlyLaser : MonoBehaviour
+	{
+		private CircuitObject m_Circuit;
 
-  [RequireComponent(typeof(BoxCollider2D), typeof(CircuitObject), typeof(AudioSource))]
-  public class NonDeadlyLaser : MonoBehaviour
-  {
-      private CircuitObject m_Circuit;
-      
-      [SerializeField]
-      private float m_MaxAlertTime = 1f;
-      private float m_AlertTimeLeft;
+		[SerializeField]
+		private float m_MaxAlertTime = 1f;
+		private float m_AlertTimeLeft;
 
-      private void Awake() { m_Circuit = GetComponent<CircuitObject>(); }
+		private void Awake() { m_Circuit = GetComponent<CircuitObject>(); }
 
-      public void TurnOn()
-      {
-          GetComponent<BoxCollider2D>().enabled = true;
-          GetComponent<SpriteRenderer>().enabled = true;
-      }
+		public void TurnOn()
+		{
+			GetComponent<BoxCollider2D>().enabled = true;
+			GetComponent<SpriteRenderer>().enabled = true;
+		}
 
-      public void TurnOff()
-      {
-          GetComponent<BoxCollider2D>().enabled = false;
-          GetComponent<SpriteRenderer>().enabled = false;
-      }
+		public void TurnOff()
+		{
+			GetComponent<BoxCollider2D>().enabled = false;
+			GetComponent<SpriteRenderer>().enabled = false;
+		}
 
-      private void OnTriggerEnter2D(Collider2D collision)
-      {
-          // if(!collision.CompareTag("Player"))
-          //     return;
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if(collision.GetComponent<Projectile>())
+				return;
 
-          if(m_AlertTimeLeft <= 0)
-              StartCoroutine(TriggerLaser());
-      }
+			if(m_AlertTimeLeft <= 0)
+				StartCoroutine(TriggerLaser());
+		}
 
-      private void OnTriggerStay2D(Collider2D collision)
-      {
-          if(!collision.CompareTag("Player"))
-              return;
+		private void OnTriggerStay2D(Collider2D collision)
+		{
+			if(collision.GetComponent<Projectile>())
+				return;
 
-          m_AlertTimeLeft = m_MaxAlertTime;
-      }
+			m_AlertTimeLeft = m_MaxAlertTime;
+		}
 
-      private IEnumerator TriggerLaser()
-      {
-          m_AlertTimeLeft = m_MaxAlertTime;
+		private IEnumerator TriggerLaser()
+		{
+			m_AlertTimeLeft = m_MaxAlertTime;
 
-          if(!m_Circuit.active)
-              m_Circuit.TriggerStateChange(CircuitState.Positive);
+			if(!m_Circuit.active)
+				m_Circuit.TriggerStateChange(CircuitState.Positive);
 
-          while(m_AlertTimeLeft > 0)
-          {
-              m_AlertTimeLeft -= Time.fixedDeltaTime;
-              yield return new WaitForFixedUpdate();
-          }
+			while(m_AlertTimeLeft > 0)
+			{
+				m_AlertTimeLeft -= Time.fixedDeltaTime;
+				yield return new WaitForFixedUpdate();
+			}
 
-          if(m_Circuit.active)
-              m_Circuit.TriggerStateChange(CircuitState.Off);
+			if(m_Circuit.active)
+				m_Circuit.TriggerStateChange(CircuitState.Off);
 
-          yield return null;
-      }
-  }
-  
+			yield return null;
+		}
+	}
 }
