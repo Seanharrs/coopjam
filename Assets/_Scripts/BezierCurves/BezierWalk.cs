6bbb1d3b949e368ffbe5cb94a269b2f89b997046
James Marks <jamesgmarks@gmail.com>
2018-06-05 13:36:27 -0400

    Splines
    * BezierWalk now automatically sets its position based on spline start position at edit time.
    * Separate spline prefab has been added. (Not just only for moving platforms anymore.)

--- Assets/_Scripts/BezierCurves/BezierWalk.cs
+++ Assets/_Scripts/BezierCurves/BezierWalk.cs
@@ -2,66 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SplineWalkerMode
+namespace Coop
 {
-  Once,
-  Loop,
-  PingPong
-}
-
-public class BezierWalk : MonoBehaviour {
-
-  public BezierSpline spline;
-
-  public bool lookForward = false;
-  public SplineWalkerMode mode;
-  private bool goingForward = true;
-
-  public float duration;
-
-  private float progress;
-
-  private void Update()
+  public enum SplineWalkerMode
   {
-    if (goingForward)
+    Once,
+    Loop,
+    PingPong
+  }
+
+  public class BezierWalk : MonoBehaviour
+  {
+
+    public BezierSpline spline;
+
+    public bool lookForward = false;
+    public SplineWalkerMode mode;
+    private bool goingForward = true;
+
+    public float duration;
+
+    [SerializeField, Range(0f, 1f)]
+    public float startAt;
+    private float progress;
+
+    private void Awake()
     {
-      progress += Time.deltaTime / duration;
-      if (progress > 1f)
+      progress = startAt;
+    }
+
+    private void Update()
+    {
+      if (goingForward)
       {
-        if (mode == SplineWalkerMode.Once)
+        progress += Time.deltaTime / duration;
+        if (progress > 1f)
         {
-          progress = 1f;
-        }
-        else if (mode == SplineWalkerMode.Loop)
-        {
-          progress -= 1f;
-        }
-        else
-        {
-          progress = 2f - progress;
-          goingForward = false;
+          if (mode == SplineWalkerMode.Once)
+          {
+            progress = 1f;
+          }
+          else if (mode == SplineWalkerMode.Loop)
+          {
+            progress -= 1f;
+          }
+          else
+          {
+            progress = 2f - progress;
+            goingForward = false;
+          }
         }
       }
-    }
-    else
-    {
-      progress -= Time.deltaTime / duration;
-      if (progress < 0f)
+      else
       {
-        progress = -progress;
-        goingForward = true;
+        progress -= Time.deltaTime / duration;
+        if (progress < 0f)
+        {
+          progress = -progress;
+          goingForward = true;
+        }
       }
-    }
-    Vector3 position = spline.GetPoint(progress);
-    // transform.position = position;
-    var rb = GetComponent<Rigidbody2D>();
-    //var vel = rb.velocity;
-    //position = Vector2.SmoothDamp(transform.position, position, ref vel, .01f);
-    rb.MovePosition(position);
-    
-    if (lookForward)
-    {
-      transform.LookAt(position + spline.GetDirection(progress));
+      Vector3 position = spline.GetPoint(progress);
+      var rb = GetComponent<Rigidbody2D>();
+      if(rb)
+        rb.MovePosition(position);
+      else
+        transform.position = position;
+      //position = Vector2.SmoothDamp(transform.position, position, ref vel, .01f);
+
+      if (lookForward)
+      {
+        transform.LookAt(position + spline.GetDirection(progress));
+      }
     }
   }
 }
