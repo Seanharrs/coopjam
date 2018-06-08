5d6369574a8f29d0b26c7b6a07f1a9a49744508f
James Marks <jamesgmarks@gmail.com>
2018-06-08 13:10:12 -0400

    Added more organized platform ruletile tileset and updated appropriate tools/objects with new tile.

---
+++ Assets/_Scripts/AutoBreakPrefabConnection.cs
@@ -0,0 +1,15 @@
+ // AutoBreakPrefabConnection.cs
+ 
+ using UnityEngine;
+ 
+ [ExecuteInEditMode]
+ public class AutoBreakPrefabConnection : MonoBehaviour
+ {
+     void Start()
+     {
+         #if UNITY_EDITOR
+          UnityEditor.PrefabUtility.DisconnectPrefabInstance(gameObject);
+         #endif
+         DestroyImmediate(this); // Remove this script
+     }
+ }
